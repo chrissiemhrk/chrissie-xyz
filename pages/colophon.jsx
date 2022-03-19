3639aabd7dabd4793e972180811382e2ed6145b1
chrissiemhrk <chrissiemhrk@gmail.com>
2022-03-19 13:30:03 +0200

    add colophon page

---
+++ pages/colophon.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function colophon() {
+  return (
+    <section className="mt-52">
+      <p>
+        This website is built using VS Code, statically generated through
+        Next.js, and deployed using Netlify. The source code that powers it can
+        be found on GitHub. The content on this website is copyrighted.
+      </p>
+    </section>
+  )
+}
+
+export default colophon
