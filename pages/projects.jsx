2122bc2fde0bd1f4739abc656be62df4f60ad5b7
chrissiemhrk <chrissiemhrk@gmail.com>
2022-03-19 13:05:00 +0200

    add projects

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -3,11 +3,30 @@ import React from 'react'
 const projects = () => {
   return (
     <section className="pb-10">
-      <h3>Below is a collection of some of projects I’ve worked on.</h3>
-      intego advertising. freelance. design + development. Technical: Figma +
-      wix kalisana. freelance. design + development. Technical: Figma +
-      wordpress 2u money. ossix technologies. frontend development.Technical:
-      ReactJs + NextJs + TypeScript + JavaScript
+      <h3 className="mb-10">
+        Below is a collection of some of projects I work/worked on.
+      </h3>
+      <a
+        href="https://2u.money/"
+        className="flex items-center justify-between cursor-pointer border-b border-stone-700 py-3"
+      >
+        2u money • Ossix Technologies • Frontend Development • Technical:
+        ReactJs + NextJs + TypeScript + JavaScript • https://2u.money/
+      </a>
+      <a
+        href="https://www.kalisana.be/"
+        className="flex items-center justify-between cursor-pointer border-b border-stone-700 py-3"
+      >
+        Kalisana • Freelance • Design + Development • Technical: Figma +
+        WordPress • https://www.kalisana.be/
+      </a>
+      <a
+        href="https://www.integoadvertising.com/"
+        className="flex items-center justify-between cursor-pointer border-b border-stone-700 py-3"
+      >
+        Intego advertising • Freelance • Design + Development • Technical: Figma
+        + Wix • https://www.integoadvertising.com/
+      </a>
     </section>
   )
 }
