aace49d3f6c3d4cb0f192f9f274d547eee888f3d
chrissiemhrk <chrissiemhrk@gmail.com>
2022-03-19 14:43:47 +0200

    change body text font weight

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -15,7 +15,7 @@ const postPage = ({ frontmatter: { title, date }, slug, content }) => {
   return (
     <section className="blog-post pb-12 flex-row">
       <Link href="/blog">
-        <a className="font-light lg:fixed left-80">&#8592; Blog</a>
+        <a className="  lg:fixed left-80">&#8592; Blog</a>
       </Link>
       <h1 className="text-3xl mb-2 mt-10 lg:mt-0 font-normal">{title}</h1>
       <small className="text-stone-300">Posted on {date}</small>
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,13 +5,13 @@ const index = () => {
     <section className="pb-10">
       <h1 className="text-3xl font-semibold mb-10">Chrissie Muhorakeye</h1>
       <section>
-        <p className="mb-5 font-light">
+        <p className="mb-5  ">
           I've always loved computers and the internet, ever since I was a kid.
           I was in high school when I built my first program for a Computer
           Science class. I fell in love with it and I decided right then and
           there that programming would be my future.
         </p>
-        <p className="mb-5 font-light">
+        <p className="mb-5  ">
           The first language I learned was Scratch, and after that came C and
           Python. After taking a few classes in high school, though, I ran into
           a problem: the curriculum didn't cover web development at all. So I
@@ -19,7 +19,7 @@ const index = () => {
           upon FreeCodeCamp, where I taught myself the fundamentals of frontend
           web development.
         </p>
-        <p className="mb-5 font-light">
+        <p className="mb-5  ">
           Now I work as a frontend developer at Ossix Technologies, where we're
           building 2u Money — a financial platform that lets users send money to
           anybody, anywhere in Africa. In my free time, when I'm not reading
